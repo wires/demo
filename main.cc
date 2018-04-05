6bbf536a081874b50c0fced91de5821ca0a9e6f7
Arian van Putten <aeroboy94@gmail.com>
2018-04-05 21:52:35 +0200

    Add shader to main.cc

--- main.cc
+++ main.cc
@@ -8,6 +8,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <chrono>
 
 #define GL_GLEXT_PROTOTYPES 1
 #define GL3_PROTOTYPES 1
@@ -28,7 +29,7 @@ static unsigned int fragmentShader;
 static unsigned int shaderProgram;
 
 static const char* vertexShaderSource = R"(
-#version 440
+#version 420
 layout (location = 0) in vec3 aPos;
 
 void main()
@@ -38,12 +39,50 @@ void main()
 )";
 
 static const char* fragmentShaderSource = R"(
-#version 440
+#version 420
+#extension GL_ARB_explicit_uniform_location : enable
 out vec4 FragColor;
+layout (location = 0) uniform float iTime;
 
-void main()
-{
-    FragColor = vec4(1.0f, 1.0f, 0.1f, 1.0f);
+float rand(vec2 co) {
+  return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
+float noise(float prev, in vec2 fragCoord) {
+  float noiseS = sin(8.4 * fragCoord.y) / 2.0;
+  return prev * 0.9 + 0.1 * noiseS;
+}
+
+vec4 vhs(in vec2 fragCoord) {
+  vec4 baseBase = vec4(0.2, 0.3, 0.8, 1.0);
+
+  vec2 base2 = round(0.5 + 0.5 * sin(sin(iTime) * cos(iTime) + 0.9 * cos(fragCoord)));
+  float baseColor = base2.x + base2.y;
+  vec4 base4 = vec4(vec3(baseColor), 1.0);
+  vec4 base = base4 * vec4(0.50, 0.49, 0.20, 1.0);
+
+  float y = 0.1 * fragCoord.y + 41.0 * iTime;
+  float n = pow(sin(y * 0.1) * 0.6 + 0.6, 5.0);
+  vec4 stripes = vec4(0.9 * n, 0.3 * n, 0.97 * n, 1.0);
+
+  float noize = pow(rand(iTime / 5000.0 + fragCoord), 6.0);
+  noize = noise(noize, fragCoord);
+  noize = noise(noize, fragCoord);
+  noize = noise(noize, fragCoord);
+  noize = noise(noize, fragCoord);
+  noize = noise(noize, fragCoord);
+  noize = noise(noize, fragCoord);
+  noize = noise(noize, fragCoord);
+
+  float noize2 = noise(noize, fragCoord);
+
+  return (0.9 * noize2 * stripes) + 0.3 * noize;
+}
+
+
+void main() {
+    FragColor = 0.2 * vec4(1.0f, 1.0f, 0.1f, 1.0f);
+    FragColor += vhs(gl_FragCoord.xy);
 }
 )";
 
@@ -77,9 +116,11 @@ void setup() {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 }
 
+float iTime = 0.0;
 void render() {
   glClear(GL_COLOR_BUFFER_BIT);
   glUseProgram(shaderProgram);
+  glUniform1f(0, iTime);
   glBindVertexArray(vao);
   glDrawArrays(GL_TRIANGLES, 0, 3);
 }
@@ -90,10 +131,14 @@ void reportError(GLenum, GLenum, GLuint, GLenum severity, GLsizei length, const 
   if (severity == GL_DEBUG_SEVERITY_HIGH || severity == GL_DEBUG_SEVERITY_MEDIUM) abort();
 }
 
+void kutzooi(int argc, const char *error) {
+  printf("%s", error);
+}
 int main(int argc, char** argv) {
   GLFWwindow* window;
 
-  if (!glfwInit()) return -1;
+  glfwSetErrorCallback(kutzooi);
+  if (!glfwInit()) return -2;
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -103,7 +148,7 @@ int main(int argc, char** argv) {
 
   if (!window) {
     glfwTerminate();
-    return -1;
+    return -3;
   }
 
   glfwMakeContextCurrent(window);
@@ -130,6 +175,7 @@ int main(int argc, char** argv) {
 
   while (!glfwWindowShouldClose(window)) {
     render();
+    iTime += 0.01;
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
