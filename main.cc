4cfab38fd45482a43a28971e4723f8de81c1f9a8
Ruud van Asseldonk <dev@veniogames.com>
2018-04-05 20:21:11 +0200

    Hello, triangle!
    Add basic OpenGL program with debug printing that aborts if anything is
    wrong. No frees, this is not a proper program.

--- main.cc
+++ main.cc
@@ -1,17 +1,95 @@
 #include <cstdio>
+#include <cstdlib>
+
+#define GL_GLEXT_PROTOTYPES 1
+#define GL3_PROTOTYPES 1
+
+#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
-const char * shader = R"(
+static float vertices[] = {
+  -0.5f, -0.5f, 0.0f,
+   0.5f, -0.5f, 0.0f,
+   0.0f,  0.5f, 0.0f
+};
 
-  void main() {
-  }
+static unsigned int vbo;
+static unsigned int vao;
+static unsigned int vertexShader;
+static unsigned int fragmentShader;
+static unsigned int shaderProgram;
 
-  )";
+static const char* vertexShaderSource = R"(
+#version 440
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+)";
+
+static const char* fragmentShaderSource = R"(
+#version 440
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 1.0f, 0.1f, 1.0f);
+}
+)";
+
+void setup() {
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glGenVertexArrays(1, &vao);
+
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+  glCompileShader(vertexShader);
+
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+  glCompileShader(fragmentShader);
+
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+}
+
+void render() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glUseProgram(shaderProgram);
+  glBindVertexArray(vao);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void reportError(GLenum, GLenum, GLuint, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+  fprintf(stderr, "%s\n", message);
+
+  if (severity == GL_DEBUG_SEVERITY_HIGH || severity == GL_DEBUG_SEVERITY_MEDIUM) abort();
+}
 
 int main(int argc, char** argv) {
   GLFWwindow* window;
 
   if (!glfwInit()) return -1;
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   window = glfwCreateWindow(1280, 720, "Demo", nullptr, nullptr);
 
@@ -22,8 +100,13 @@ int main(int argc, char** argv) {
 
   glfwMakeContextCurrent(window);
 
+  glDebugMessageCallback(reportError, nullptr);
+  glEnable(GL_DEBUG_OUTPUT);
+
+  setup();
+
   while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
+    render();
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
